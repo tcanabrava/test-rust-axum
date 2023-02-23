56985352208f7ac30a4f315b46598dde9b68ddb0
Tomaz Canabrava <tcanabrava@kde.org>
2023-02-23 10:24:09 +0100

    Add reqwest and make unittests work

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,9 @@
-
-use axum::{
-    routing::get,
-    Router,
-    extract::Path,
-};
-
-use hyper::StatusCode;
-
-async fn hello_world() -> &'static str {
-    "Hello World\n"
-}
-
-async fn greet(Path(user_name): Path<String>) -> String {
-    let res = format!("Hello {}\n", user_name);
-    res
-}
-
-async fn health_check() -> StatusCode {
-    StatusCode::OK
-}
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/",  get(hello_world))
-        .route("/greet/:user_name", get(greet))
-        .route("/health_check", get(health_check));
-    
-    let addr: std::net::SocketAddr = "0.0.0.0:3000".parse().unwrap();
-
-    let server = axum::Server::bind(&addr)
-        .serve(app.into_make_service());
-
+    println!("Starting the server");
+    let server = run();
     server.await.unwrap();
+    println!("Server finished");
 }
