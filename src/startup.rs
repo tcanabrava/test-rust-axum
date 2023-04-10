089d11d8819801841c0ca6882749e7c2f721d678
Tomaz Canabrava <tcanabrava@kde.org>
2023-04-10 19:13:52 +0200

    Add an id to every call so we can trace what happened

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,7 @@
 use axum::{
     routing::{get, post, IntoMakeService},
     Router,
+    body::Body
 };
 
 use hyper::server::conn::AddrIncoming;
@@ -12,15 +13,36 @@ use crate::{
     state::AppState,
 };
 
+use tower_http::trace::TraceLayer;
+use tower_request_id::{RequestId, RequestIdLayer};
+use http::Request;
+
 pub fn run(
     listener: TcpListener,
     state: AppState,
 ) -> axum::Server<AddrIncoming, IntoMakeService<Router>> {
-        let app = Router::new()
+    let trace_layer = TraceLayer::new_for_http()
+        .make_span_with(|request: &Request<Body>| {
+        let request_id = request
+            .extensions()
+            .get::<RequestId>()
+            .map(ToString::to_string)
+            .unwrap_or_else(|| "unknown".into());
+
+        tracing::error_span!(
+            "request",
+            id = %request_id,
+            method = %request.method(),
+            uri = %request.uri(),
+        )
+    });
+
+    let app = Router::new()
         .route("/health_check", get(health_check))
         .route("/subscribe", post(subscribe))
         .with_state(state)
-        .layer(tower_http::trace::TraceLayer::new_for_http());
+        .layer(trace_layer)
+        .layer(RequestIdLayer);
 
     let server = axum::Server::from_tcp(listener)
         .expect("Could not bind to TcpListener")
